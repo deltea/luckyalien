9b5546567308c5f2393ed4521225e8dbb85ec327
Leo <thcheetah777@gmail.com>
2021-07-09 09:53:24 -0700

    Create a config file

---
+++ config.js
@@ -0,0 +1,33 @@
+// Phaser config
+const config = {
+  // Type
+  type: Phaser.AUTO,
+
+  // Proportions
+  width: 1300,
+	height: 643,
+
+  // Color of sky
+  backgroundColor: 0xd0f4f7,
+
+  // Physics
+  physics: {
+    // Default
+    default: "arcade",
+
+    // Arcade
+    arcade: {
+      // Gravity
+      gravity: {
+        y: 1500
+      },
+
+      // Options
+      enableBody: true,
+      // debug: true
+    }
+  },
+
+  // Scenes
+  scene: [Grassland, Boss, SignContent, Forest, Clouds, Credits]
+};
--- script.js
+++ script.js
@@ -44,40 +44,5 @@ class Scene extends Phaser.Scene {
   }
 }
 
-// Scenes
-// Phaser config
-const config = {
-  // Type
-  type: Phaser.AUTO,
-
-  // Proportions
-  width: 1300,
-	height: 643,
-
-  // Color of sky
-  backgroundColor: 0xd0f4f7,
-
-  // Physics
-  physics: {
-    // Default
-    default: "arcade",
-
-    // Arcade
-    arcade: {
-      // Gravity
-      gravity: {
-        y: 1500
-      },
-
-      // Options
-      enableBody: true,
-      // debug: true
-    }
-  },
-
-  // Scenes
-  scene: [Grassland, Boss, SignContent, Forest, Clouds, Credits]
-};
-
 // Phaser game
 const phaserGame = new Phaser.Game(config);

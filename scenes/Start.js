b6633365f9504deec51a1cdd7aeb61243107af5d
Leo <thcheetah777@gmail.com>
2021-07-13 15:04:20 -0700

    Create scene structure

---
+++ scenes/Start.js
@@ -0,0 +1,22 @@
+// Start scene
+class Start extends Phaser.Scene {
+  // Constructor
+  constructor() {
+    super("Start");
+  }
+
+  // Load assets
+  preload() {
+
+  }
+
+  // Main create function
+  create() {
+
+  }
+
+  // Update animations, sprites
+  update() {
+    
+  }
+}
